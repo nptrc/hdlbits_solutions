a354f3519c88afd380a5c05c9bc6e174659f1b79
nptrc <khoa05032006@gmail.com>
2026-07-17 11:34:38 +0700

    add: inverter

---
+++ Verilog_Language/Basics/inverter/tb.v
@@ -0,0 +1,27 @@
+`timescale 1ns / 1ps
+
+module tb;
+
+  reg in;
+  reg out;
+
+  top_module dut (
+      .in (in),
+      .out(out)
+  );
+
+  initial begin
+    $dumpfile("dump.vcd");
+    $dumpvars(0, tb);
+    $monitor("in=%b out=%b", in, out);
+
+    in = 0;
+    #10;
+
+    in = 1;
+    #10;
+
+    $finish;
+  end
+
+endmodule
