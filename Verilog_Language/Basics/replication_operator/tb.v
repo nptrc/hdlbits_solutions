510b9247d752ee63b2095d92734dc86d0633da87
nptrc <khoa05032006@gmail.com>
2026-07-18 22:01:52 +0700

    add: replication_operator

---
+++ Verilog_Language/Basics/replication_operator/tb.v
@@ -0,0 +1,29 @@
+`timescale 1ns / 1ps
+
+module tb;
+
+  reg  [ 7:0] in;
+  wire [31:0] out;
+
+  top_module dut (
+      .in (in),
+      .out(out)
+  );
+
+  initial begin
+    $dumpfile("dump.vcd");
+    $dumpvars(0, tb);
+
+    in = 8'b10101010;
+    #10;
+
+    in = 8'b11110000;
+    #10;
+
+    in = 8'b01101100;
+    #10;
+
+    $finish;
+  end
+
+endmodule
