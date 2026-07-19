28252f3247d27ad36cbf72b6a65008f0ea0237c3
nptrc <khoa05032006@gmail.com>
2026-07-19 15:44:26 +0700

    add: three_modules

---
+++ Verilog_Language/Modules/three_modules/top_module.v
@@ -0,0 +1,10 @@
+module top_module (
+    input  clk,
+    input  d,
+    output q
+);
+  wire q1, q2;
+  my_dff ff1 ( .clk(clk), .d (d), .q(q1) );
+  my_dff ff2 ( .clk(clk), .d(q1), .q(q2) );
+  my_dff ff3 ( .clk(clk), .d(q2), .q(q)  );
+endmodule
